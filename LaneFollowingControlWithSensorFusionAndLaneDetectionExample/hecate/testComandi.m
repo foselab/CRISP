b7f7cfca2bb98891b17160b25bd45accb25c9aa0
lcarminati13 <l.carminati13@studenti.unibg.it>
2024-10-02 12:03:51 +0200

    modifica testComandi
    adesso sono in grado di generare la stepTable e la getFit funziona

--- LaneFollowingControlWithSensorFusionAndLaneDetectionExample/hecate/testComandi.m
+++ LaneFollowingControlWithSensorFusionAndLaneDetectionExample/hecate/testComandi.m
@@ -1,6 +1,12 @@
+
 modelName = "LaneFollowingTestBenchExample"; 
+%% SIMCONFIG. Prendo il test assessment
 assSet = sltest.getAssessments(modelName); 
 as = get(assSet, 1);
 testAssessmentPath = string(as.BlockPath.getBlock(1));
+% in realtà questo comando mi da un indirizzo sbagliato, quindi nella
+% GETALLSTEPS ho inserito manuamente il percorso per il test assessment
 
-stepTable = getAllSteps("LaneFollowingTestBenchExample", testAssessmentPath); 
+%% GETALLSTEPS
+stepTable = getAllSteps('Scenario_1', 'LFRefMdl/Test Assessment'); 
+[stepTableTA, fitTable] = getFit(stepTable);
