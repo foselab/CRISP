8cdda8f901f2961d6ddf3bec31ac3b291374e89b
lcarminati13 <l.carminati13@studenti.unibg.it>
2025-06-03 16:04:05 +0200

    versione 2.0

--- LaneFollowingControlWithSensorFusionAndLaneDetectionExample/Run_All_Configurations.m
+++ LaneFollowingControlWithSensorFusionAndLaneDetectionExample/Run_All_Configurations.m
@@ -93,7 +93,7 @@ for i = 1 : length_vehicles_array
 end 
 
 % salvo i dati in un file excel 
-writetable(Results_Table, 'tabellarisultati_TEST_V5_CONF_0.xlsx');
+writetable(Results_Table, 'tabellarisultati_TEST_V7_CONF_0.xlsx');
 
 tempo_trascorso = toc;  % Ferma il timer e salva il tempo trascorso
 disp(['Tempo impiegato: ', num2str(tempo_trascorso), ' secondi']);
--- LaneFollowingControlWithSensorFusionAndLaneDetectionExample/Run_One_Configuration.m
+++ LaneFollowingControlWithSensorFusionAndLaneDetectionExample/Run_One_Configuration.m
@@ -4,9 +4,9 @@ tic;  % Inizia il timer
 % configurazione parametri, si inserisce il nome del file dell'automobile e il nome del file dello scenario 
 modelname_simulation = 'LaneFollowingTestBenchExample';
 fprintf('INIZIO CONFIGURAZIONE PARAMETRI AUTO E SCENARIO\n');
-VEHICLE_NAME = 'A4.m'; 
-SCENARIO_NAME = 'LFACC_04_Curve_CutInOut';
-id_scenario = 2;  % ricordarsi di cambiare anche questo
+VEHICLE_NAME = 'Tcross.m'; 
+SCENARIO_NAME = 'Anaconda';
+id_scenario = 5;  % ricordarsi di cambiare anche questo
 run(VEHICLE_NAME);
 fprintf('PARAMETRI CORRETTAMENTE CONFIGURATI\n');
 
