3518d4cb4e48287c24f6a9b32e598323f706b484
lcarminati13 <l.carminati13@studenti.unibg.it>
2025-06-04 19:08:01 +0200

    modifiche test assessment DEFINITIVO
    tornato alla versione con solo il test sulla distanza

--- LaneFollowingControlWithSensorFusionAndLaneDetectionExample/Run_One_Configuration.m
+++ LaneFollowingControlWithSensorFusionAndLaneDetectionExample/Run_One_Configuration.m
@@ -17,7 +17,7 @@ fprintf('HECATE CONFIGURATO CORRETTAMENTE\n');
 
 %inizio simulazione
 fprintf('INIZIO SIMULAZIONE\n');
-helperLFSetUp(max_acceleration, min_acceleration, 0.01, -0.01, total_mass, yaw, long_distance_front, long_distance_rear, cornering_stiff_front, cornering_stiff_rear, tau, SCENARIO_NAME, id_scenario);
+helperLFSetUp(max_acceleration, min_acceleration, max_steering, min_steering, total_mass, yaw, long_distance_front, long_distance_rear, cornering_stiff_front, cornering_stiff_rear, tau, SCENARIO_NAME, id_scenario);
 [Out] = sim(modelname_simulation, 'ReturnWorkspaceOutputs', 'on');
 fprintf('SIMULAZIONE CONCLUSA\n');
 
