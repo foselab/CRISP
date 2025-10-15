6b69ba775dea0194f8621ddb26ebb65d78fecb44
lcarminati13 <l.carminati13@studenti.unibg.it>
2025-10-15 14:43:11 -0400

    change

--- LaneFollowingControlWithSensorFusionAndLaneDetectionExample/grafico_medio.m
+++ LaneFollowingControlWithSensorFusionAndLaneDetectionExample/grafico_medio.m
@@ -1,5 +1,5 @@
 %% CREAZIONE GRAFICO MEDIO 
-T1 = readtable('tabellarisultati_RANDOM1_CONF_2.xlsx');
+T1 = readtable('tabellarisultatiRandom_CONF1_RUN_1_finale.xlsx');
 vettore_failure_1 = double(T1{1:end, 3}); % estraggo la colonna delle collisioni escludendo la prima riga di intestazione
 vettore_failure_binario_1 = zeros(1,length(vettore_failure_1));
 for i=1:length(vettore_failure_1)
@@ -10,7 +10,7 @@ end
 
 vettore_failure_binario_1 = cumsum(vettore_failure_binario_1);
 
-T2 = readtable('tabellarisultati_RANDOM2_CONF_2.xlsx');
+T2 = readtable('tabellarisultatiRandom_CONF1_RUN_2_finale.xlsx');
 vettore_failure_2 = double(T2{1:end, 3}); % estraggo la colonna delle collisioni escludendo la prima riga di intestazione
 vettore_failure_binario_2 = zeros(1,length(vettore_failure_2));
 for i=1:length(vettore_failure_2)
@@ -21,7 +21,7 @@ end
 
 vettore_failure_binario_2 = cumsum(vettore_failure_binario_2);
 
-T3 = readtable('tabellarisultati_RANDOM3_CONF_2.xlsx');
+T3 = readtable('tabellarisultatiRandom_CONF1_RUN_3_finale.xlsx');
 vettore_failure_3 = double(T3{1:end, 3}); % estraggo la colonna delle collisioni escludendo la prima riga di intestazione
 vettore_failure_binario_3 = zeros(1,length(vettore_failure_3));
 for i=1:length(vettore_failure_3)
@@ -32,7 +32,7 @@ end
 
 vettore_failure_binario_3 = cumsum(vettore_failure_binario_3);
 
-T4 = readtable('tabellarisultati_RANDOM4_CONF_2.xlsx');
+T4 = readtable('tabellarisultatiRandom_CONF1_RUN_4_finale.xlsx');
 vettore_failure_4 = double(T4{1:end, 3}); % estraggo la colonna delle collisioni escludendo la prima riga di intestazione
 vettore_failure_binario_4 = zeros(1,length(vettore_failure_4));
 for i=1:length(vettore_failure_4)
@@ -43,7 +43,7 @@ end
 
 vettore_failure_binario_4 = cumsum(vettore_failure_binario_4);
 
-T5 = readtable('tabellarisultati_RANDOM5_CONF_2.xlsx');
+T5 = readtable('tabellarisultatiRandom_CONF1_RUN_5_finale.xlsx');
 vettore_failure_5 = double(T5{1:end, 3}); % estraggo la colonna delle collisioni escludendo la prima riga di intestazione
 vettore_failure_binario_5 = zeros(1,length(vettore_failure_5));
 for i=1:length(vettore_failure_5)
@@ -81,11 +81,11 @@ xlabel('Simulazioni');
 ylabel('Failure');
 title('Grafico performance RANDOM MEDIO');
 grid on;
-filename = fullfile('C:\Users\Luca\Desktop\tesi ACC\carminati\LaneFollowingControlWithSensorFusionAndLaneDetectionExample\grafici\OTA2','grafico_random_medio_CONF2.fig');
+filename = fullfile('C:\Users\Luca\Desktop\tesi ACC\carminati\LaneFollowingControlWithSensorFusionAndLaneDetectionExample\grafici\OTA1','grafico_random_medio_CONF1.fig');
 savefig(filename);
 
 %% grafico sovrapposto 
-T_fitness = readtable('tabellarisultati_HECATE_CONF_2.xlsx');
+T_fitness = readtable('tabellarisultatiHecate_CONF1_finale.xlsx');
 vettore_failure = double(T_fitness{1:end, 3}); % estraggo la colonna delle collisioni escludendo la prima riga di intestazione
 vettore_failure_binario_FITNESS = zeros(1,length(vettore_failure));
 for i=1:length(vettore_failure)
@@ -96,7 +96,7 @@ end
 
 vettore_failure_binario_FITNESS = cumsum(vettore_failure_binario_FITNESS);
 
-T_APDISC = readtable('tabellarisultati_APDISC_CONF2.xlsx');
+T_APDISC = readtable('tabellarisultatiHecate_CONF1_APDISC.xlsx');
 vettore_failure = double(T_APDISC{1:end, 3}); % estraggo la colonna delle collisioni escludendo la prima riga di intestazione
 vettore_failure_binario_APDISC = zeros(1,length(vettore_failure));
 for i=1:length(vettore_failure)
@@ -118,9 +118,9 @@ p(3).LineWidth=2;
 
 yticks(0:1:max(vettore_failure_medio));
 
-xlabel('Simulazioni');
-ylabel('Failure');
+xlabel('Simulation index');
+ylabel('Cumulative faults');
 title('FITNESS (green) vs RANDOM (red) vs APDISC (blue)');
 grid on;
-filename = fullfile('C:\Users\Luca\Desktop\tesi ACC\carminati\LaneFollowingControlWithSensorFusionAndLaneDetectionExample\grafici\OTA2','grafico_CONFRONTO_CONF2.fig');
+filename = fullfile('C:\Users\Luca\Desktop\tesi ACC\carminati\LaneFollowingControlWithSensorFusionAndLaneDetectionExample\grafici\OTA1','grafico_CONFRONTO_CONF1.fig');
 savefig(filename);
